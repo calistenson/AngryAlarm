b90b7abbb814c1f421fdd12a142b05ffeeb6a36e
clarasmith <clara@smithsaltzman.com>
2013-12-18 10:09:19 -0500

    Reorganizing

--- GUI/OpenDialogTester.java
+++
@@ -1,8 +0,0 @@
-
-
-public class OpenDialogTester {
-  public static void main (String[] args) {
-    OpenDialog newdialog = new OpenDialog(null, "Clraa");
-    //JOptionPane.showMessageDialog(null, content);
-  }
-}
